5683ee32d8fd97c74fdb786110a6717fcda44603
PetreCatalin <catalin_petre2007@yahoo.com>
2016-12-12 17:21:33 +0200

    Add files via upload

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -56,8 +56,72 @@ public partial class _Default : System.Web.UI.Page
         HtmlGenericControl div = new HtmlGenericControl("div");
         HtmlGenericControl imagine = new HtmlGenericControl("img");
         imagine.Attributes.Add("src", photos[0]); */
-        
-        con.Close();
 
+        sport1img.Src = "~/Imagini/" + photos[0];
+        sport1h.InnerHtml = titles[0];
+        sport1ad.InnerHtml = "Stire adaugata la " + dates[0];
+        sport2img.Src = "~/Imagini/" + photos[1];
+        sport2h.InnerHtml = titles[1];
+        sport2ad.InnerHtml = "Stire adaugata la " + dates[1];
+
+
+
+        //categoria stiinta
+        string query1 = "SELECT Id,Titlu,Poza,DataAdaugare FROM Stiri WHERE Categorie='Stiinta' ORDER BY DataAdaugare DESC";
+        SqlCommand cmd1 = new SqlCommand(query1, con);
+        SqlDataReader queryCommandReader1 = cmd1.ExecuteReader();
+
+        DataTable dataTable1 = new DataTable();
+        dataTable1.Load(queryCommandReader1);
+
+        int numarLinii1 = dataTable1.Rows.Count;
+        int[] ids1 = new int[numarLinii1 + 5];
+        string[] titles1 = new string[numarLinii1 + 5];
+        string[] photos1 = new string[numarLinii1 + 5];
+        DateTime[] dates1 = new DateTime[numarLinii1 + 5];
+
+        for (int i = 0; i < numarLinii1; i++)
+        {
+            ids1[i] = (int)dataTable1.Rows[i]["Id"];
+            titles1[i] = (String)dataTable1.Rows[i]["Titlu"];
+            photos1[i] = (String)dataTable1.Rows[i]["Poza"];
+            dates1[i] = (DateTime)dataTable1.Rows[i]["DataAdaugare"];
+           // Response.Write(ids1[i] + " " + titles1[i] + " " + photos1[i] + " " + dates1[i]+" ");
+        }
+
+        stiinta1img.Src = "~/Imagini/" + photos1[0];
+        stiinta1h.InnerHtml = titles1[0];
+        stiinta1ad.InnerHtml = "Stire adaugata la " + dates1[0];
+
+
+
+        //categoria tehnologie
+        string query2 = "SELECT Id,Titlu,Poza,DataAdaugare FROM Stiri WHERE Categorie='Tehnologie' ORDER BY DataAdaugare DESC";
+        SqlCommand cmd2 = new SqlCommand(query2, con);
+        SqlDataReader queryCommandReader2 = cmd2.ExecuteReader();
+
+        DataTable dataTable2 = new DataTable();
+        dataTable2.Load(queryCommandReader2);
+
+        int numarLinii2 = dataTable2.Rows.Count;
+        int[] ids2 = new int[numarLinii2 + 5];
+        string[] titles2 = new string[numarLinii2 + 5];
+        string[] photos2 = new string[numarLinii2 + 5];
+        DateTime[] dates2 = new DateTime[numarLinii2 + 5];
+
+        for (int i = 0; i < numarLinii2; i++)
+        {
+            ids2[i] = (int)dataTable2.Rows[i]["Id"];
+            titles2[i] = (String)dataTable2.Rows[i]["Titlu"];
+            photos2[i] = (String)dataTable2.Rows[i]["Poza"];
+            dates2[i] = (DateTime)dataTable2.Rows[i]["DataAdaugare"];
+            // Response.Write(ids1[i] + " " + titles1[i] + " " + photos1[i] + " " + dates1[i]+" ");
+        }
+
+        tehnologie1img.Src = "~/Imagini/" + photos2[0];
+        tehnologie1h.InnerHtml = titles2[0];
+        tehnologie1ad.InnerHtml = "Stire adaugata la " + dates2[0];
+
+        con.Close();
     }
 }
